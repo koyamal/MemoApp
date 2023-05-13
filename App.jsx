0c3b391968d5d54229dd72e925869c8ea44c86f5
koyamal <tanakamaruko1121n@gmail.com>
2023-05-13 10:26:29 +0900

    Add shadow to CircleButton

--- App.jsx
+++ App.jsx
@@ -102,6 +102,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 40,
     bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
   },
   circleButtonLabel: {
     color: '#ffffff',
