f9bfe06c75b94b25d132086c3b3e1af4f2fe33d3
koyamal <tanakamaruko1121n@gmail.com>
2023-05-13 11:06:32 +0900

    Add style to MemoDetailScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -14,20 +14,20 @@ export default function MemoDetailScreen() {
     <View style={styles.container}>
       <AppBar />
 
-      <View>
-        <Text>買い物リスト1</Text>
-        <Text>2023年05月12日 10:00</Text>
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト1</Text>
+        <Text style={styles.memoDate}>2023年05月12日 10:00</Text>
       </View>
 
-      <ScrollView>
-        <Text>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>
           買い物リスト1
           testtesttesttesttesttesttest
           testtesttest
         </Text>
       </ScrollView>
 
-      <CircleButton>+</CircleButton>
+      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
     </View>
   );
 }
@@ -35,6 +35,32 @@ export default function MemoDetailScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f4f8',
+    backgroundColor: '#ffffff',
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
   },
 });
