f44e3f7a7fe20d7aa4892a5def51613210ed6d6b
koyamal <tanakamaruko1121n@gmail.com>
2023-05-14 17:27:22 +0900

    Create SignUpScreen

--- App.jsx
+++ App.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import LogInScreen from './src/screens/LogInScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
     // <MemoDetailScreen />
     // <MemoEditScreen />
     // <MemoCreateScreen />
-    <LogInScreen />
+    // <LogInScreen />
+    <SignUpScreen />
   );
 }
