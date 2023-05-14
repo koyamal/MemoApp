988c88501758bf5dec946adb91dcc4847aa6b8fb
koyamal <tanakamaruko1121n@gmail.com>
2023-05-14 17:24:32 +0900

    Add props to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import {
   View, Text, StyleSheet,
 } from 'react-native';
+import { string } from 'prop-types';
 
-export default function Button() {
+export default function Button(props) {
+  const { label } = props;
   return (
     <View style={styles.buttonContainer}>
-      <Text style={styles.buttonLabel}>Submit</Text>
+      <Text style={styles.buttonLabel}>{label}</Text>
     </View>
   );
 }
+
+Button.propTypes = {
+  label: string.isRequired,
+};
 
 const styles = StyleSheet.create({
   buttonContainer: {
