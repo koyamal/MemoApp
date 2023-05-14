e4c42e2fe78a178081fa48a18a83da7ed154006a
koyamal <tanakamaruko1121n@gmail.com>
2023-05-14 17:43:30 +0900

    Use TouchableOpacity for x button in MemoList

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 export default function MemoList() {
@@ -10,27 +12,27 @@ export default function MemoList() {
           <Text style={styles.memoListItemTitle}>買い物リスト1</Text>
           <Text style={styles.memoListItemDate}>2023年05月12日 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Feather name="x" size={16} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
       <View style={styles.memoListItem}>
         <View>
           <Text style={styles.memoListItemTitle}>買い物リスト2</Text>
           <Text style={styles.memoListItemDate}>2023年05月12日 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Feather name="x" size={16} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
       <View style={styles.memoListItem}>
         <View>
           <Text style={styles.memoListItemTitle}>買い物リスト3</Text>
           <Text style={styles.memoListItemDate}>2023年05月12日 10:00</Text>
         </View>
-        <View>
+        <TouchableOpacity>
           <Feather name="x" size={16} color="#B0B0B0" />
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
