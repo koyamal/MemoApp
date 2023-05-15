87171de01c835dfe2283859a0529291dcf3e6356
koyamal <tanakamaruko1121n@gmail.com>
2023-05-15 11:25:57 +0900

    Edit Position of CircleButton in MemoDetailScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -24,7 +24,7 @@ export default function MemoDetailScreen() {
         </Text>
       </ScrollView>
 
-      <CircleButton name="edit-2" style={{ top: 160, bottom: 'auto' }} />
+      <CircleButton name="edit-2" style={{ top: 60, bottom: 'auto' }} />
     </View>
   );
 }
