ff7209e1d680091ad90020bf8a84216faa38f342
koyamal <tanakamaruko1121n@gmail.com>
2023-05-15 11:39:59 +0900

    Created Page transition from MemoEdit to MemoDetail

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {
-  View, StyleSheet, TextInput, Alert,
+  View, StyleSheet, TextInput,
 } from 'react-native';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
-export default function MemoEditScreen() {
+export default function MemoEditScreen(props) {
+  const { navigation } = props;
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
-      <CircleButton name="check" onPress={() => { Alert.alert('Button Pushed'); }} />
+      <CircleButton
+        name="check"
+        onPress={() => { navigation.goBack(); }}
+      />
     </KeyboardSafeView>
   );
 }
