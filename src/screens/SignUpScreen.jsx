375bd0f4e143d004f6499aa0a37f5be63e3a2c75
koyamal <tanakamaruko1121n@gmail.com>
2023-05-15 17:01:21 +0900

    Create SignUp funcition by firebase.auth

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   View, Text, TextInput, StyleSheet, TouchableOpacity,
 } from 'react-native';
+import firebase from 'firebase';
 
 import Button from '../components/Button';
 
@@ -9,6 +10,18 @@ export default function SignUpScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function handlePress() {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user.uid);
+      });
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'MemoList' }],
+    });
+  }
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
@@ -37,12 +50,7 @@ export default function SignUpScreen(props) {
         />
         <Button
           label="Submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
+          onPress={() => handlePress()}
         />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
