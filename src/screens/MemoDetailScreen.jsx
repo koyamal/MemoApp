5ee8601b757a81dac89e56cb2ec3b1c2757ada49
koyamal <tanakamaruko1121n@gmail.com>
2023-05-15 11:19:33 +0900

    Delete Appbar from all screens

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -6,14 +6,11 @@ import {
   StyleSheet,
 } from 'react-native';
 
-import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
 
 export default function MemoDetailScreen() {
   return (
     <View style={styles.container}>
-      <AppBar />
-
       <View style={styles.memoHeader}>
         <Text style={styles.memoTitle}>買い物リスト1</Text>
         <Text style={styles.memoDate}>2023年05月12日 10:00</Text>
