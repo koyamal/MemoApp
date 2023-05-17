bb8155554e9536cc0f0f4db4c5c720e503ab5195
koyamal <tanakamaruko1121n@gmail.com>
2023-05-17 17:32:04 +0900

    Use useState for memos

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
 import firebase from 'firebase';
 
@@ -8,6 +8,8 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
+
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogOutButton />,
@@ -22,9 +24,17 @@ export default function MemoListScreen(props) {
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
       unsubscribe = ref.onSnapshot(
         (snapshot) => {
+          const userMemos = [];
           snapshot.forEach((doc) => {
-            console.log(doc.id, doc.data());
+            const data = doc.data();
+            console.log(doc.id, data);
+            userMemos.push({
+              id: doc.id,
+              bodyText: data.bodyText,
+              updatedAt: data.updatedAt,
+            });
           });
+          setMemos(userMemos);
         },
         (error) => {
           console.log(error);
