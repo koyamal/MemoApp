4c8c13a7e529fc83acba527795defb6b64d1378b
koyamal <tanakamaruko1121n@gmail.com>
2023-05-22 17:45:52 +0900

    Display bodyText in MemoEdit

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View, StyleSheet, TextInput,
 } from 'react-native';
@@ -10,11 +10,17 @@ import KeyboardSafeView from '../components/KeyboardSafeView';
 export default function MemoEditScreen(props) {
   const { navigation, route } = props;
   const { id, bodyText } = route.params;
+  const [body, setBody] = useState(bodyText);
   console.log(id, bodyText);
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput value="買い物リスト" multiline style={styles.input} />
+        <TextInput
+          value={body}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => { setBody(text); }}
+        />
       </View>
       <CircleButton
         name="check"
