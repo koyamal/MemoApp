0c9f5052ddde5847f1242ec733e4e080a827f0b0
koyamal <tanakamaruko1121n@gmail.com>
2023-05-23 18:24:28 +0900

    Create toggleStar in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,28 @@
 import { format } from 'date-fns';
+import firebase from 'firebase';
+import { Alert } from 'react-native';
 
 export function dateToString(date) {
   if (!date) {
     return '';
   }
   return format(date, 'yyyy年M月d日 HH時mm分');
+}
+
+export function toggleStar(item) {
+  if (item) {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(item.id);
+      ref.set({
+        isStar: !item.isStar,
+      }, { merge: true })
+        .then(() => {
+        })
+        .catch((error) => {
+          Alert.alert(error.code);
+        });
+    }
+  }
 }
