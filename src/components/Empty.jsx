1f2ae01ab619bf6c198c510ae82a84e75180722e
koyamal <tanakamaruko1121n@gmail.com>
2023-05-24 09:29:59 +0900

    Create Empty component

---
+++ src/components/Empty.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  View, StyleSheet, Text,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { bool } from 'prop-types';
+
+import Button from './Button';
+import Loading from './Loading';
+
+export default function Empty(props) {
+  const { isLoading } = props;
+  const navigation = useNavigation();
+  return (
+    <View style={emptyStyles.container}>
+      <Loading isLoading={isLoading} />
+      <View style={emptyStyles.inner}>
+        <Text style={emptyStyles.title}>
+          メモを作成しよう!
+        </Text>
+        <Button
+          label="作成する"
+          onPress={() => { navigation.navigate('MemoCreate'); }}
+          style={emptyStyles.button}
+        />
+      </View>
+    </View>
+  );
+}
+
+Empty.propTypes = {
+  isLoading: bool.isRequired,
+};
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
+  },
+});
