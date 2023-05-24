578cf7ab5aafff63e047d4fbef6cfffcd868db01
koyamal <tanakamaruko1121n@gmail.com>
2023-05-24 10:01:54 +0900

    Use CircleStarButton

--- src/components/StarEmpty.jsx
+++ src/components/StarEmpty.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  View, StyleSheet, Text, TouchableOpacity,
+  View, StyleSheet, Text,
 } from 'react-native';
 import { bool, func } from 'prop-types';
 
 import Loading from './Loading';
-import Star from './Star';
+import CircleStarButton from './CircleStarButton';
 
 export default function StarEmpty(props) {
   const { isLoading, setOnlyStar, onlyStar } = props;
@@ -20,14 +20,7 @@ export default function StarEmpty(props) {
           お気に入りフィルタを解除してください
         </Text>
       </View>
-      <TouchableOpacity style={starStyles.button} onPress={() => { setOnlyStar(!onlyStar); }}>
-        <Star
-          isStar={onlyStar}
-          name={['star', 'star']}
-          size={40}
-          color={['#FFCC00', '#ffffff']}
-        />
-      </TouchableOpacity>
+      <CircleStarButton setOnlyStar={setOnlyStar} onlyStar={onlyStar} />
     </View>
   );
 }
@@ -37,25 +30,6 @@ StarEmpty.propTypes = {
   setOnlyStar: func.isRequired,
   onlyStar: bool.isRequired,
 };
-
-const starStyles = StyleSheet.create({
-  button: {
-    position: 'absolute',
-    left: 40,
-    bottom: 40,
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-});
 
 const starEmptyStyles = StyleSheet.create({
   container: {
