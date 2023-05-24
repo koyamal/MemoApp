9fd9a227d65ad5db94b2179982b89a4c4e9bac4e
koyamal <tanakamaruko1121n@gmail.com>
2023-05-24 10:00:52 +0900

    Add module

--- src/components/CircleStarButton.jsx
+++ src/components/CircleStarButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import { bool, func } from 'prop-types';
 
 import Star from './Star';
