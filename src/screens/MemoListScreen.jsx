36d9da9834b9f414b4df3fad05bfd2654701a5a4
koyamal <tanakamaruko1121n@gmail.com>
2023-05-24 09:06:49 +0900

    Delete style

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -68,23 +68,6 @@ export default function MemoListScreen(props) {
     return unsubscribe;
   }, [onlyStar]);
 
-  const starStyle = {
-    position: 'absolute',
-    left: 40,
-    bottom: 40,
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  };
-
   if (memos.length === 0 && onlyStar) {
     return (
       <View style={starEmptyStyles.container}>
