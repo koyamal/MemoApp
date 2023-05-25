4eae8f8b791428f8f7e5f8038899b7b932518b75
koyamal <tanakamaruko1121n@gmail.com>
2023-05-25 13:21:24 +0900

    Add isLoading to StarProvider

--- src/components/provider/StarProvider.jsx
+++ src/components/provider/StarProvider.jsx
@@ -6,9 +6,10 @@ export const StarContext = createContext({});
 export default function StarProvider(props) {
   const { children } = props;
   const [onlyStar, setOnlyStar] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     // eslint-disable-next-line
-    <StarContext.Provider value={{ onlyStar, setOnlyStar }}>
+    <StarContext.Provider value={{ onlyStar, setOnlyStar, isLoading, setIsLoading }}>
       {children}
     </StarContext.Provider>
   );
